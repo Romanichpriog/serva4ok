61e0d7715ed42e7ff5d8e9bbc261f202dc5d459d
Roma <0>
2020-05-27 18:38:05 +0300

    2

--- src/main/java/com/serva4ok/Egator/controller/RestExerciseController.java
+++ src/main/java/com/serva4ok/Egator/controller/RestExerciseController.java
@@ -3,24 +3,27 @@ package com.serva4ok.Egator.controller;
 import com.serva4ok.Egator.domain.Exercise;
 import com.serva4ok.Egator.repos.ExerciseRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("get")
 public class RestExerciseController {
-    @Autowired
+
+
+
     private ExerciseRepo exerciseRepo;
 
-    @RequestMapping("/findbynomer")
-    public String findByNomer(@RequestParam("nomer") String nomer){
-        Iterable<Exercise> exercises = exerciseRepo.findAll();
-        String result="";
-        for(Exercise exercise: exerciseRepo.findByNomer(nomer)){
-            result+= exercise.toString()+"<br>";
-        }
-        return result;
+    @Autowired
+    public RestExerciseController(ExerciseRepo exerciseRepo) {
+        this.exerciseRepo = exerciseRepo;
+    }
+
+    @RequestMapping("{nomer}")
+    public Exercise findByNomer(@PathVariable("nomer") Exercise exercise){
+        return exercise;
+
 
     }
 
