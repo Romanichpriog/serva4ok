fadc0fb902f79252e2fc0d107106e31d834c94ef
Roma <0>
2020-05-27 08:26:42 +0300

    2

--- src/main/java/com/serva4ok/Egator/controller/ExerciseController.java
+++ src/main/java/com/serva4ok/Egator/controller/ExerciseController.java
@@ -1,35 +1,22 @@
 package com.serva4ok.Egator.controller;
 
 
-import com.serva4ok.Egator.exceptions.NotFoundException;
-import org.springframework.data.crossstore.ChangeSetPersister;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping()
 public class ExerciseController {
-    public List<Map<String,String>> exercises = new ArrayList<Map<String, String>>(){{
-        add(new HashMap<String, String>() {{put("id","1");put("text","zadanie1");}});
-        add(new HashMap<String, String>() {{put("id","2");put("text","zadanie2");}});
-        add(new HashMap<String, String>() {{put("id","3");put("text","zadanie3");}});
-    }};
-    @GetMapping
-    public List<Map<String,String>> list(){
-        return exercises;
-    }
-    @GetMapping("{id}")
-    public Map<String,String> getOne(@PathVariable String id){
-        return exercises.stream().filter(exercises-> exercises.get("id").equals(id))
-                .findFirst()
-                .orElseThrow(NotFoundException::new);
+    @GetMapping("/greetings")
+    public String greeting(
+            @RequestParam(name = "name",required = false, defaultValue = "world" )
+            String name, Model model
+    ){
+        model.addAttribute("name",name);
+        return "exercise";
     }
 
 }
---
+++ src/main/java/com/serva4ok/Egator/repos/ExerciseRepos.java
@@ -0,0 +1,7 @@
+package com.serva4ok.Egator.repos;
+
+import com.serva4ok.Egator.model.Exercise;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ExerciseRepos extends CrudRepository<Exercise,Long> {
+}
