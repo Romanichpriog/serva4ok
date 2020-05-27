ade434d72639fec9db867b59258aff99112c1cee
Roma <0>
2020-05-27 13:59:40 +0300

    2

--- src/main/java/com/serva4ok/Egator/controller/ExerciseController.java
+++ src/main/java/com/serva4ok/Egator/controller/ExerciseController.java
@@ -3,6 +3,7 @@ package com.serva4ok.Egator.controller;
 
 import com.serva4ok.Egator.domain.Exercise;
 import com.serva4ok.Egator.repos.ExerciseRepo;
+import com.sun.tools.jdeps.JdepsFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -31,7 +32,7 @@ public class ExerciseController {
         return "main";
     }
     @PostMapping
-    public String add(@RequestParam String nomer,@RequestParam String textzadania,@RequestParam String uslovie, @RequestParam String otvet, @RequestParam String text, Map<String,Object> model){
+    public String add(@RequestParam String nomer,@RequestParam String textzadania,@RequestParam(defaultValue = "") String uslovie, @RequestParam String otvet, @RequestParam(defaultValue = "") String text, Map<String,Object> model){
         Exercise exercise = new Exercise(nomer,textzadania,uslovie,otvet,text);
         exerciseRepo.save(exercise);
         Iterable<Exercise> exercises = exerciseRepo.findAll();
