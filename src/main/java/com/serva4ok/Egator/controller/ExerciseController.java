c1229c8cb8be9db3340094348859cdbdbeae8879
Roma <0>
2020-05-27 08:53:04 +0300

    2

--- src/main/java/com/serva4ok/Egator/controller/ExerciseController.java
+++ src/main/java/com/serva4ok/Egator/controller/ExerciseController.java
@@ -1,21 +1,20 @@
 package com.serva4ok.Egator.controller;
 
 
-import org.springframework.ui.Model;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping()
+import java.util.Map;
+
+@Controller
 public class ExerciseController {
     @GetMapping("/greetings")
     public String greeting(
             @RequestParam(name = "name",required = false, defaultValue = "world" )
-            String name, Model model
+            String name, Map<String,Object> model
     ){
-        model.addAttribute("name",name);
+        model.put("name",name);
         return "exercise";
     }
 
--- src/main/java/com/serva4ok/Egator/repos/ExerciseRepos.java
+++
@@ -1,7 +0,0 @@
-package com.serva4ok.Egator.repos;
-
-import com.serva4ok.Egator.model.Exercise;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ExerciseRepos extends CrudRepository<Exercise,Long> {
-}
