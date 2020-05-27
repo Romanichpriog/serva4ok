f1925c4a69b4f1f1a523a75d215ddd24ac310f2f
Roma <0>
2020-05-27 09:33:51 +0300

    2

--- src/main/java/com/serva4ok/Egator/controller/ExerciseController.java
+++ src/main/java/com/serva4ok/Egator/controller/ExerciseController.java
@@ -2,20 +2,27 @@ package com.serva4ok.Egator.controller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Map;
 
 @Controller
+
 public class ExerciseController {
     @GetMapping("/greetings")
     public String greeting(
-            @RequestParam(name = "name",required = false, defaultValue = "world" )
-            String name, Map<String,Object> model
+            @RequestParam(name="name", required = false,defaultValue = "world")  String name, Map<String,Object> model
     ){
         model.put("name",name);
         return "exercise";
     }
+    @GetMapping
+    public String main(Map<String,Object> model){
+        model.put("some","Hello, lets code");
+        return "main";
+    }
+
 
 }
